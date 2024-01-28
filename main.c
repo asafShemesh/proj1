9a4d156f0d237529adbf53331695bfad126d2e27
asafShemesh <119500206+asafShemesh@users.noreply.github.com>
2024-01-28 22:17:35 +0200

    main.c

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ int main() {
     printf("The Armstrong numbers are:");
          for (int i = num1; i <= num2; ++i) {
                if (isArmstrong(i)) {
-            printf("%d ", i);
+            printf( " %d", i);
           }
          }
     printf("\n");
     printf("The Palindromes are:");
     for (int i = num1; i <= num2; ++i) {
         if (isPalindrome(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -27,7 +27,7 @@ int main() {
     printf("The Prime numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isPrime(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -36,7 +36,7 @@ int main() {
     printf("The Strong numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isStrong(i)) {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     
