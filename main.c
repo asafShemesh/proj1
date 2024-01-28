472ac11835370f5b3f8e4bf823500373b86185c4
asafShemesh <119500206+asafShemesh@users.noreply.github.com>
2024-01-28 22:07:09 +0200

    main.c

--- main.c
+++ main.c
@@ -4,37 +4,18 @@
 int main() {
     int num1, num2;
 
-    printf("Enter the first positive integer: ");
+    
     scanf("%d", &num1);
-
-    printf("Enter the second positive integer: ");
     scanf("%d", &num2);
-
-    printf("Prime numbers between %d and %d:\n", num1, num2);
-    for (int i = num1; i <= num2; ++i) {
-        if (isPrime(i)) {
+    
+    printf("The Armstrong numbers are:", num1, num2);
+         for (int i = num1; i <= num2; ++i) {
+               if (isArmstrong(i)) {
             printf("%d ", i);
-        }
-    }
+          }
+         }
     printf("\n");
-
-    printf("Armstrong numbers between %d and %d:\n", num1, num2);
-    for (int i = num1; i <= num2; ++i) {
-        if (isArmstrong(i)) {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
-    printf("Strong numbers between %d and %d:\n", num1, num2);
-    for (int i = num1; i <= num2; ++i) {
-        if (isStrong(i)) {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
-    printf("Palindromes between %d and %d:\n", num1, num2);
+    printf("The Palindromes are:", num1, num2);
     for (int i = num1; i <= num2; ++i) {
         if (isPalindrome(i)) {
             printf("%d ", i);
@@ -42,5 +23,22 @@ int main() {
     }
     printf("\n");
 
+
+    printf("The Prime numbers are:", num1, num2);
+    for (int i = num1; i <= num2; ++i) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    
+    printf("The Strong numbers are:", num1, num2);
+    for (int i = num1; i <= num2; ++i) {
+        if (isStrong(i)) {
+            printf("%d ", i);
+        }
+    }
+    
     return 0;
 }
